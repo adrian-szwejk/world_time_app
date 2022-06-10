2f97585f1319c18b142e6cfdfbef16f66c84f5b1
adrian-szwejk <szwejkowski.adrian@gmail.com>
2022-06-09 23:50:35 -0500

    added stateful widget inside every screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:world_time_app/pages/home.dart';
+import 'package:world_time_app/pages/choose_location.dart';
+import 'package:world_time_app/pages/loading.dart';
 
 void main() {
-  runApp(const MaterialApp());
+  runApp(const MaterialApp(
+    home: Home(),
+  ));
 }
