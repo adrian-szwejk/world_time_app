3772ee7a68980ea703c8839d8afa1f5d7b9b6b4e
adrian-szwejk <szwejkowski.adrian@gmail.com>
2022-06-10 19:26:57 -0500

    added function to home scrn button to nav to loc

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,12 @@ import 'package:world_time_app/pages/choose_location.dart';
 import 'package:world_time_app/pages/loading.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Home(),
+  runApp(MaterialApp(
+    initialRoute: "/home",
+    routes: {
+      "/": (context) => const Loading(),
+      "/home": (context) => const Home(),
+      "/location": (context) => const ChooseLocation(),
+    },
   ));
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -10,9 +10,22 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SafeArea(child: Text("Home Screen")),
+    return Scaffold(
       //SafeArea widget makes sure it is visible for the user(within bounds of screen)
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            TextButton.icon(
+              onPressed: () {
+                //Navigator widget is a function used to push a different screen on top of current one with a name in this case (stacking screens)
+                Navigator.pushNamed(context, "/location");
+              },
+              icon: const Icon(Icons.edit_location),
+              label: const Text("Edit Location"),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
