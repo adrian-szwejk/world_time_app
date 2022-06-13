09e090dea817bd98571dcdd6ca3638e560b9261a
adrian-szwejk <szwejkowski.adrian@gmail.com>
2022-06-12 21:52:41 -0500

    added spinner to replace text in loading screen

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'package:world_time_app/services/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class Loading extends StatefulWidget {
   const Loading({Key? key}) : super(key: key);
@@ -37,10 +38,14 @@ class _LoadingState extends State<Loading> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(30.0),
-        child: Text("loading"),
+    return Scaffold(
+      backgroundColor: Colors.lightBlue[200],
+      body: const Center(
+        //Spinning circle to replace loading text to show while app is loading up
+        child: SpinKitFadingCircle(
+          color: Colors.white,
+          size: 100.0,
+        ),
       ),
     );
   }
